82717a5217cc819d31e134530d376b209e9188ed
njiang <njiang@telenav.cn>
2012-08-21 15:27:59 +0800

    Add get all environment common function
    Signed-off-by: njiang <njiang@telenav.cn>

--- EnvManagement/views.py
+++ EnvManagement/views.py
@@ -1,10 +1,3 @@
-from EnvManagement.models import environment
+from DBUtils import *
 
-#session = environment(env_name='DEV_FP',server='hqd-fptn72csvr-01',username='tnuser',password='tndev',tomcat_path='/usr/local/apache-tomcat-6.0.20',zipfolder_path='/home/tnuser/zipFolder',service_locator_path='/home/tnuser/serviceLocator')
-#session.save()
-
-def getEnv():
-    dbValue = environment.objects.all()
-    for env in dbValue:
-        print env.id
 
