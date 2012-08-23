d9a82323d14bccbaf9256f8b411c6e8062c3f6d2
Attis Wong <qjwang2@telenav.cn>
2012-08-23 18:15:24 +0800

    add cli web lookup function
    Signed-off-by: Attis Wong <qjwang2@telenav.cn>

---
+++ Cli/service/cli_service.py
@@ -0,0 +1,32 @@
+'''
+Created on 2012-8-20
+
+@author: qjwang2
+'''
+from Cli.service.SSHHelper import SSHHelper
+from EnvManagement.DBUtils import getAllEnvs
+
+cli_file_path = '/usr/local/tnconfig/cli_client.properties'
+
+
+def getCliInfoByEnv(env):
+    sshHelper = SSHHelper(env)
+    sshHelper.loginSSH()
+    stdout = sshHelper.executeComand('cat %s' % cli_file_path)
+    dictList = []
+    for line in stdout:
+        if(line):
+            if(line.find('=')>-1):
+                dictList.append(line.strip().split('='))
+    
+    clidict = dict(dictList)
+    clidict['ServerIp'] = sshHelper.pingDNS(clidict['Server'])
+    sshHelper.logoutSSH()
+    return clidict
+
+def getEnvList():
+    return getAllEnvs()
+    
+def getCliInfoList(envList):
+    cliInfoList = [getCliInfoByEnv(env) for env in envList]
+    return cliInfoList
