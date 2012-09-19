cc86e85e4161b2495978a11766aaf30e64c27fdc
kwwang <kwwang@telenav.cn>
2012-09-19 16:41:14 +0800

    add environment list and new_and_edit html file

---
+++ CserverToolsSuite/static/js/cts.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$("#cancel_save_env").click(function ()
+			{
+				history.go(-1);
+			});
+});
--- CserverToolsSuite/view_test.py
+++ CserverToolsSuite/view_test.py
@@ -8,8 +8,10 @@ from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
 
 
+mock_env=environment(id=111,env_name='dev_fp',server='172.16.10.101',username="tnuser",password="tndev",tomcat_path="/home/tnuser/tomcat",zipfolder_path="/home/tnuser/zipfolder",service_locator_path="/home/tnuser/servicelocator")
+
 def list_env(request,template):
-    env_list=[environment(id=111,env_name='dev_fp',server='172.16.10.101',username="tnuser",password="tndev",tomcat_path="/home/tnuser/tomcat",zipfolder_path="/home/tnuser/zipfolder",service_locator_path="/home/tnuser/servicelocator")]
+    env_list=[mock_env]
     return render_to_response(template,{'env_list':env_list})
 
 def new_env(request,template):
@@ -18,3 +20,10 @@ def new_env(request,template):
 def save_env(request,template):
     print request.POST.get("password")
     return render_to_response(template)
+
+def edit_env(request,envID,template):
+    return render_to_response(template,{'env':mock_env})
+
+def delete_env(request,envID,template):
+    print "delete environment"
+    return render_to_response(template,{'env':mock_env})
